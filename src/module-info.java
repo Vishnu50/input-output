688901b92ecd11148cbc06d4bed30ce09c67063f
vishnu <vishnu.v>
2021-08-18 19:27:27 +0530

    code change

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+import java .util.scanner
+Public class Main {
+	public static void Main (string[] args) {
+	double my gpa=8.0;
+	string my firstname="vishnu";
+	string my lastname="venkat";
+	system.out.println(my firstname+ "" +my lastname);
+	sysytem.out.println("what do you want to update it to");
+	input=new scanner(system.in);
+	my gpa=input.nextDouble();
+	system.out.println(my firstname+ "" +my lastname + my gpa);
+	}
